e22664e7392190274b759672d0371476e1260391
Meysam Mahmoudi <meisamdev@gmail.com>
2024-11-12 14:17:28 +0330

    Add activation and deactivation hooks

--- my-brizy-plugin.php
+++ my-brizy-plugin.php
@@ -6,3 +6,24 @@
  * Author: MeisamDev
  * License: GPL2
  */
+
+ // Activation and deactivation hooks
+register_activation_hook( __FILE__, 'my_brizy_plugin_activate' );
+register_deactivation_hook( __FILE__, 'my_brizy_plugin_deactivate' );
+
+/**
+ * Plugin activation callback.
+ * Adds rewrite rules and flushes rewrite rules.
+ */
+function my_brizy_plugin_activate() {
+    my_brizy_plugin_add_rewrite_rules();
+    flush_rewrite_rules();
+}
+
+/**
+ * Plugin deactivation callback.
+ * Flushes rewrite rules.
+ */
+function my_brizy_plugin_deactivate() {
+    flush_rewrite_rules();
+}
