9e733c28b97ba51eb222d02f5d44495ae326a62b
Meysam Mahmoudi <meisamdev@gmail.com>
2024-11-12 14:17:13 +0330

    Add plugin header information

---
+++ my-brizy-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: My Brizy Plugin
+ * Description: Adds custom routes to return head and body HTML content for a given post ID.
+ * Version: 1.0.0
+ * Author: MeisamDev
+ * License: GPL2
+ */
